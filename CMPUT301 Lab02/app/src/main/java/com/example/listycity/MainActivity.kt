983b0285676bd24cc12634de8c104ff87f99685d
mjaxin <mpjackso@ualberta.ca>
2026-09-08 18:40:22 -0600

    Done walkthrough

--- CMPUT301 Lab02/app/src/main/java/com/example/listycity/MainActivity.kt
+++ CMPUT301 Lab02/app/src/main/java/com/example/listycity/MainActivity.kt
@@ -4,20 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.listycity.ui.theme.ListyCityTheme
 import androidx.compose.ui.unit.dp
-
+import androidx.compose.runtime.setValue
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +38,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     CityListScreen(
                         cities = cityRepository.cities,
+                        onAddCity = { cityRepository.addCity(it)},
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -36,27 +47,39 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-class CityRepository {
-    private val _cities = mutableStateListOf(
-        "Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin",
-        "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"
-    )
-
-    val cities: List<String>
-        get() = _cities
-
-    fun addCity(city: String) {
-        _cities.add(city)
-    }
-}
-
 @Composable
-fun CityListScreen(cities: List<String>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier.fillMaxSize()) {
-        items(cities) { city ->
-            CityRow(city = city)
+fun CityListScreen(cities: List<String>, onAddCity : (String) -> Unit, modifier: Modifier = Modifier) {
+    var newCityName by remember { mutableStateOf("")}
+    Column(modifier = modifier.fillMaxSize()) {
+        Row(modifier = Modifier.padding(16.dp)) {
+            OutlinedTextField(
+                value = newCityName,
+                onValueChange = { newCityName = it},
+                label = { Text("City name")},
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Button(
+                onClick = {
+                    if(newCityName.isNotBlank()) {
+                        onAddCity(newCityName)
+                        newCityName = ""
+
+                    }
+                }
+            ) {
+                Text("Add City")
+            }
+        }
+        LazyColumn(modifier = modifier.fillMaxSize()) {
+            items(cities) { city ->
+                CityRow(city = city)
+            }
         }
     }
+
+
 }
 @Composable
 fun CityRow(city: String) {
@@ -78,5 +101,19 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     ListyCityTheme {
         Greeting("Android")
+    }
+}
+
+class CityRepository {
+    private val _cities = mutableStateListOf(
+        "Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin",
+        "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"
+    )
+
+    val cities: List<String>
+        get() = _cities
+
+    fun addCity(city: String) {
+        _cities.add(city)
     }
 }
